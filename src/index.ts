f4d3caaac86f3480aecb9fe976cb1cdb73d99786
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2021-02-19 07:52:56 +0100

    docs: ✏️ geerate + gh-pages
    Auto generate docs and add workflow to push to gh-pages branch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
 /**
- * Starting point.
+ *
+ *
+ * @export
+ * @param {number} foo
+ * @param {string} truc
+ * @param {boolean} [bar]
+ * @return {*}  {string}
  */
-export default {}
+export function doSomething(foo: number, truc: string, bar?: boolean): string {
+    if (bar) {
+        return truc + foo
+    }
+    throw new Error('No way.')
+}
